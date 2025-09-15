150dff88744990d566d5ae61ef4bfefc3cde89b8
SanketTops <sanket.tops@gmail.com>
2025-09-15 16:22:38 +0530

    String

---
+++ Assignemnts/Module-1/string.py
@@ -0,0 +1,52 @@
+mystr="this is python!"
+
+"""print(mystr)
+print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #range - string slice
+print(mystr[5:])
+print(mystr[:4])"""
+
+#-----------------------------#
+
+"""print(mystr)
+print(len(mystr))"""
+
+#-----------------------------#
+#String Methods
+print(mystr)
+#print(mystr.strip())
+#print(mystr.lower())
+#print(mystr.upper())
+#print(mystr.replace('This','That'))
+#print(mystr.replace('T','Y'))
+#print(mystr.split())
+#print(mystr.split('i'))
+#print(mystr.capitalize())
+#print(mystr.casefold())
+#print(mystr.count('i'))
+#print(mystr.startswith('This'))
+
+"""if mystr.startswith("THIS"):
+    print("Yes...Match done!")
+else:
+    print("Error!")"""
+    
+#print(mystr.endswith("!"))
+
+#print(mystr.find('P'))
+#print(mystr.index('P'))
+#print(mystr.isalpha())
+#print(mystr.isdigit())
+#print(mystr.isalnum())
+#print(mystr.islower())
+#print(mystr.isupper())
+#print(mystr.title())
+
+
+em="\U0001F604"
+print(em)   
+
+
+
+    
