de1ff3f699628b9fb874689b86209eef5468b263
SanketTops <sanket.tops@gmail.com>
2025-09-17 16:25:01 +0530

    List

---
+++ Assignemnts/Module-2_Collection/list_example.py
@@ -0,0 +1,59 @@
+data=['python','java','php','c#','kotlin']
+"""print(data)
+print(data[0])
+print(data[-1])
+print(data[0:3])
+print(data[2:])
+print(data[:4])
+print(len(data))"""
+
+#--------------------------------#
+
+"""print(data)
+data[3]='html' #value update
+print(data)"""
+
+#--------------------------------#
+
+"""print(data)
+if 'JAVA' in data:
+    print("Yes...")
+else:
+    print("Noo...")"""
+    
+#--------------------------------#
+#print(data)
+
+"""for i in data:
+    print(i)"""
+
+#--------------------------------#
+#print(data)
+#data.append("css")
+#data.insert(2,"html")
+#data.remove('php')
+#data.pop()
+#data.pop(1)
+#data.clear()
+#del data
+#data.reverse()
+#data.sort()
+#print(data)
+
+"""newdata=data.copy()
+print(newdata)"""
+
+#newdata=["c","c++","sql"]
+#print(newdata)
+#print(data+newdata)
+
+#data.extend(newdata)
+#print(data)
+
+
+# ---------------------------- #
+print(data)
+#print(data.index("kotlin"))
+
+for i in data:
+    print(data.index(i))
