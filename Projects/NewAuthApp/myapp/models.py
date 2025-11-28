cff1f6aa0802971ed40aeb45a7d649d73aa073a0
SanketTops <sanket.tops@gmail.com>
2025-11-28 12:50:54 +0530

    Full Auth

---
+++ Projects/NewAuthApp/myapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class UserSignup(models.Model):
+    firstname=models.CharField(max_length=20)
+    lastname=models.CharField(max_length=20)
+    email=models.EmailField()
+    password=models.CharField(max_length=15)
---
+++ Projects/NewAuthApp/myapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render,redirect
+from .forms import *
+from .models import *
+from django.contrib.auth import logout
+
+# Create your views here.
+def login(request):
+    if request.method=='POST':
+        unm=request.POST["email"]
+        pas=request.POST["password"]
+        
+        user=UserSignup.objects.filter(email=unm,password=pas)
+        if user:
+            print("Login Successfully!")
+            request.session["user"]=unm
+            
+            return redirect("home")
+        else:
+            print("Error!Login Faild...")
+    return render(request,'login.html')
+
+def signup(request):
+    if request.method=='POST':
+        newuser=SignupForm(request.POST)
+        if newuser.is_valid():
+            newuser.save()
+            print("Signup Successfully!")
+            return redirect("/")
+        else:
+            print(newuser.errors)
+    return render(request,'signup.html')
+
+def home(request):
+    user=request.session.get("user")
+    return render(request,'home.html',{'user':user})
+
+def userlogout(request):
+    logout(request)
+    return redirect("/")
