b4749af8788b3a620d21e205cb02a9835dce0b63
SanketTops <sanket.tops@gmail.com>
2025-12-01 12:57:32 +0530

    Auth with Validation

--- Projects/NewAuthApp/myapp/views.py
+++ Projects/NewAuthApp/myapp/views.py
@@ -20,19 +20,31 @@ def login(request):
     return render(request,'login.html')
 
 def signup(request):
+    msg=""
     if request.method=='POST':
+        email=request.POST["email"]
         newuser=SignupForm(request.POST)
         if newuser.is_valid():
-            newuser.save()
-            print("Signup Successfully!")
-            return redirect("/")
+            if UserSignup.objects.filter(email=email).exists():
+                msg="Email is alreasy exists!"
+            else:
+                newuser.save()
+                print("Signup Successfully!")
+                return redirect("/")
         else:
             print(newuser.errors)
-    return render(request,'signup.html')
+    return render(request,'signup.html',{'msg':msg})
 
 def home(request):
-    user=request.session.get("user")
-    return render(request,'home.html',{'user':user})
+    cuser=request.session.get("user")
+    try:
+        uname=UserSignup.objects.get(email=cuser)
+        print("Name:",uname.firstname)
+        return render(request,'home.html',{'cuser':uname.firstname})
+    except UserSignup.DoesNotExist:
+        print("Error")
+        return render(request,'home.html')
+    
 
 def userlogout(request):
     logout(request)
