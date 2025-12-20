7f1e19ec53ae1cf49e39bb49e426ab7a6e6a4dbb
SanketTops <sanket.tops@gmail.com>
2025-12-20 12:14:09 +0530

    req.txt

--- Projects/FinalProject/FinalProject/settings.py
+++ Projects/FinalProject/FinalProject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0(4&m9u4ro&@@rril43fi%b!iht7436bc4zd=t*(^cf4pxa!kg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
